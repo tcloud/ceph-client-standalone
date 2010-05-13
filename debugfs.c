937369cee30d5b13476dbf2e698a6302783c40e3
Sage Weil <sage@newdream.net>
2010-05-13 16:31:41 -0700

    backport
    Mainline commit 519a3987eb9dfd70689eb6d66b5ac6cb52385a6b
    
    Conflicts:
    
    	super.c

--- debugfs.c
+++ debugfs.c
@@ -440,9 +440,11 @@ int ceph_debugfs_client_init(struct ceph_client *client)
 	if (!client->debugfs_congestion_kb)
 		goto out;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 32)
 	sprintf(name, "../../bdi/%s", dev_name(client->sb->s_bdi->dev));
 	client->debugfs_bdi = debugfs_create_symlink("bdi", client->debugfs_dir,
 						     name);
+#endif
 
 	return 0;
 
@@ -453,7 +455,9 @@ out:
 
 void ceph_debugfs_client_cleanup(struct ceph_client *client)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 32)
 	debugfs_remove(client->debugfs_bdi);
+#endif
 	debugfs_remove(client->debugfs_caps);
 	debugfs_remove(client->debugfs_dentry_lru);
 	debugfs_remove(client->debugfs_osdmap);
--- super.c
+++ super.c
@@ -132,7 +132,11 @@ static int default_congestion_kb(void)
 	 * This allows larger machines to have larger/more transfers.
 	 * Limit the default to 256M
 	 */
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 32)
 	congestion_kb = (16*int_sqrt(totalram_pages)) << (PAGE_SHIFT-10);
+#else
+	congestion_kb = (16*int_sqrt(num_physpages)) << (PAGE_SHIFT-10);
+#endif
 	if (congestion_kb > 256*1024)
 		congestion_kb = 256*1024;
 
@@ -207,7 +211,11 @@ struct kmem_cache *ceph_cap_cachep;
 struct kmem_cache *ceph_dentry_cachep;
 struct kmem_cache *ceph_file_cachep;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27)
 static void ceph_inode_init_once(void *foo)
+#else
+static void ceph_inode_init_once(struct kmem_cache *cachep, void *foo)
+#endif
 {
 	struct ceph_inode_info *ci = foo;
 	inode_init_once(&ci->vfs_inode);
@@ -262,13 +270,28 @@ static void destroy_caches(void)
  * ceph_umount_begin - initiate forced umount.  Tear down down the
  * mount, skipping steps that may hang while waiting for server(s).
  */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 26)
+static void ceph_umount_begin(struct vfsmount *vfsmnt, int flags)
+#else
 static void ceph_umount_begin(struct super_block *sb)
+#endif
 {
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 26)
+	struct ceph_client *client = ceph_sb_to_client(vfsmnt->mnt_sb);
+#else
 	struct ceph_client *client = ceph_sb_to_client(sb);
+#endif
 
 	dout("ceph_umount_begin - starting forced umount\n");
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 26)
+	if (!(flags & MNT_FORCE))
+		return;
+#endif
+
 	if (!client)
 		return;
+
 	client->mount_state = CEPH_MOUNT_SHUTDOWN;
 	return;
 }
@@ -745,7 +768,11 @@ static struct dentry *open_root_dentry(struct ceph_client *client,
 		    client->sb->s_root == NULL)
 			root = d_alloc_root(req->r_target_inode);
 		else
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 28)
 			root = d_obtain_alias(req->r_target_inode);
+#else
+			root = d_alloc_anon(req->r_target_inode);
+#endif
 		req->r_target_inode = NULL;
 		dout("open_root_inode success, root dentry is %p\n", root);
 	} else {
@@ -914,17 +941,23 @@ static atomic_long_t bdi_seq = ATOMIC_INIT(0);
 
 static int ceph_register_bdi(struct super_block *sb, struct ceph_client *client)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 26)
 	int err;
+#endif
 
 	/* set ra_pages based on rsize mount option? */
 	if (client->mount_args->rsize >= PAGE_CACHE_SIZE)
 		client->backing_dev_info.ra_pages =
 			(client->mount_args->rsize + PAGE_CACHE_SIZE - 1)
 			>> PAGE_SHIFT;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 26)
 	err = bdi_register(&client->backing_dev_info, NULL, "ceph-%d",
 			   atomic_long_inc_return(&bdi_seq));
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 32)
 	if (!err)
 		sb->s_bdi = &client->backing_dev_info;
+#endif
+#endif
 	return err;
 }
 
@@ -998,6 +1031,12 @@ static void ceph_kill_sb(struct super_block *s)
 	dout("kill_sb %p\n", s);
 	ceph_mdsc_pre_umount(&client->mdsc);
 	kill_anon_super(s);    /* will call put_super after sb is r/o */
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 26)
+# if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 32)
+	if (s->s_bdi == &client->backing_dev_info)
+# endif
+		bdi_unregister(&client->backing_dev_info);
+#endif
 	ceph_destroy_client(client);
 }
 
