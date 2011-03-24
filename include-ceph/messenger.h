62b31f40e68dccf591b11c8e83a3cdeb5e9dc228
Henry C Chang <henry_c_chang@tcloudcomputing.com>
2011-03-24 08:07:24 +0000

    Revert "libceph: fix msgr backoff"
    Since we stayed with old workqueue mechanism, we do not need this
    fix. This reverts commit 1a03e5d3de778c9597c5d7d98cfbc1686eff982d.

--- include-ceph/messenger.h
+++ include-ceph/messenger.h
@@ -128,7 +128,6 @@ struct ceph_msg_pos {
 #define SOCK_CLOSED	11 /* socket state changed to closed */
 #define OPENING         13 /* open connection w/ (possibly new) peer */
 #define DEAD            14 /* dead, about to kfree */
-#define BACKOFF         15
 
 /*
  * A single connection with another host.
