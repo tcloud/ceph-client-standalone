6dc6e9a973a70e5966c79842bc80269e68d936f1
Yehuda Sadeh <yehuda@hq.newdream.net>
2011-07-22 11:12:28 -0700

    ceph: set up readahead size when rsize is not passed
    This should improve the default read performance, as without it
    readahead is practically disabled.
    
    Signed-off-by: Yehuda Sadeh <yehuda@hq.newdream.net>

--- ceph/super.c
+++ ceph/super.c
@@ -841,6 +841,10 @@ static int ceph_register_bdi(struct super_block *sb,
 		fsc->backing_dev_info.ra_pages =
 			(fsc->mount_options->rsize + PAGE_CACHE_SIZE - 1)
 			>> PAGE_SHIFT;
+	else
+		fsc->backing_dev_info.ra_pages =
+			default_backing_dev_info.ra_pages;
+
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 26)
 	err = bdi_register(&fsc->backing_dev_info, NULL, "ceph-%d",
 			   atomic_long_inc_return(&bdi_seq));
